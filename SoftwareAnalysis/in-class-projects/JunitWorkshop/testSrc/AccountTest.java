283968e06c64b8d210e7b6e23bbfe9d84bc50d46
cheyulin <yche@cse.ust.hk>
2016-09-23 14:57:16 +0800

    add junit study

---
+++ SoftwareAnalysis/in-class-projects/JunitWorkshop/testSrc/AccountTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cheyulin on 9/23/16.
+ */
+public class AccountTest {
+    @Test
+    public void init() throws Exception {
+
+    }
+
+    @Test
+    public void print() throws Exception {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
---
+++ SoftwareAnalysis/in-class-projects/JunitWorkshop/testSrc/SavingsAccountTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cheyulin on 9/23/16.
+ */
+public class SavingsAccountTest extends Account {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void init() {
+
+    }
+}
