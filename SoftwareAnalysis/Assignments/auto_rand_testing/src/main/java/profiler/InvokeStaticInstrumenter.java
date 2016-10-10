9b88d7960554911d44b22ffcdb3e46be766fa561
CHE Yulin <yche@cse.ust.hk>
2016-10-10 22:44:44 +0800

    update codes

--- SoftwareAnalysis/Assignments/auto_rand_testing/src/main/java/profiler/InvokeStaticInstrumenter.java
+++ SoftwareAnalysis/Assignments/auto_rand_testing/src/main/java/profiler/InvokeStaticInstrumenter.java
@@ -22,108 +22,110 @@ package profiler;/*
 /* InvokeStaticInstrumenter extends the abstract class BodyTransformer,
  * and implements <pre>internalTransform</pre> method.
  */
+
 import soot.*;
 import soot.jimple.*;
 import soot.util.*;
+
 import java.util.*;
 
 public class InvokeStaticInstrumenter extends BodyTransformer {
 
-	/* some internal fields */
-	static SootClass counterClass;
-	static SootMethod increaseCounter, reportCounter;
+    /* some internal fields */
+    static SootClass counterClass;
+    static SootMethod increaseCounter, reportCounter;
 
-	static {
-		counterClass = Scene.v().loadClassAndSupport("MyCounter");
-		increaseCounter = counterClass.getMethod("void increase(int)");
-		reportCounter = counterClass.getMethod("void report()");
-		Scene.v().setSootClassPath(null);
-	}
+    static {
+        counterClass = Scene.v().loadClassAndSupport("MyCounter");
+        increaseCounter = counterClass.getMethod("void increase(int)");
+        reportCounter = counterClass.getMethod("void report()");
+        Scene.v().setSootClassPath(null);
+    }
 
-	/*
-	 * internalTransform goes through a method body and inserts counter
-	 * instructions before an INVOKESTATIC instruction
-	 */
-	protected void internalTransform(Body body, String phase, Map options) {
-		// body's method
-		SootMethod method = body.getMethod();
+    /*
+     * internalTransform goes through a method body and inserts counter
+     * instructions before an INVOKESTATIC instruction
+     */
+    protected void internalTransform(Body body, String phase, Map options) {
+        // body's method
+        SootMethod method = body.getMethod();
 
-		// debugging
-		System.out.println("instrumenting method : " + method.getSignature());
+        // debugging
+        System.out.println("instrumenting method : " + method.getSignature());
 
-		// get body's unit as a chain
-		Chain units = body.getUnits();
+        // get body's unit as a chain
+        Chain units = body.getUnits();
 
-		// get a snapshot iterator of the unit since we are going to
-		// mutate the chain when iterating over it.
-		//
-		Iterator stmtIt = units.snapshotIterator();
+        // get a snapshot iterator of the unit since we are going to
+        // mutate the chain when iterating over it.
+        //
+        Iterator stmtIt = units.snapshotIterator();
 
-		// typical while loop for iterating over each statement
-		while (stmtIt.hasNext()) {
+        // typical while loop for iterating over each statement
+        while (stmtIt.hasNext()) {
 
-			// cast back to a statement.
-			Stmt stmt = (Stmt) stmtIt.next();
+            // cast back to a statement.
+            Stmt stmt = (Stmt) stmtIt.next();
 
-			// there are many kinds of statements, here we are only
-			// interested in statements containing InvokeStatic
-			// NOTE: there are two kinds of statements may contain
-			// invoke expression: InvokeStmt, and AssignStmt
-			if (!stmt.containsInvokeExpr()) {
-				continue;
-			}
+            // there are many kinds of statements, here we are only
+            // interested in statements containing InvokeStatic
+            // NOTE: there are two kinds of statements may contain
+            // invoke expression: InvokeStmt, and AssignStmt
+            if (!stmt.containsInvokeExpr()) {
+                continue;
+            }
 
-			// take out the invoke expression
-			InvokeExpr expr = (InvokeExpr) stmt.getInvokeExpr();
+            // take out the invoke expression
+            InvokeExpr expr = (InvokeExpr) stmt.getInvokeExpr();
 
-			// now skip non-static invocations
-			if (!(expr instanceof StaticInvokeExpr)) {
-				continue;
-			}
+            // now skip non-static invocations
+            if (!(expr instanceof StaticInvokeExpr)) {
+                continue;
+            }
 
-			// now we reach the real instruction
-			// call Chain.insertBefore() to insert instructions
-			//
-			// 1. first, make a new invoke expression
-			InvokeExpr incExpr = Jimple.v().newStaticInvokeExpr(
-					increaseCounter.makeRef(), IntConstant.v(1));
-			// 2. then, make a invoke statement
-			Stmt incStmt = Jimple.v().newInvokeStmt(incExpr);
+            // now we reach the real instruction
+            // call Chain.insertBefore() to insert instructions
+            //
+            // 1. first, make a new invoke expression
+            InvokeExpr incExpr = Jimple.v().newStaticInvokeExpr(
+                    increaseCounter.makeRef(), IntConstant.v(1));
+            // 2. then, make a invoke statement
+            Stmt incStmt = Jimple.v().newInvokeStmt(incExpr);
 
-			// 3. insert new statement into the chain
-			// (we are mutating the unit chain).
-			units.insertBefore(incStmt, stmt);
-		}
+            // 3. insert new statement into the chain
+            // (we are mutating the unit chain).
+            units.insertBefore(incStmt, stmt);
+        }
 
-		// Do not forget to insert instructions to report the counter
-		// this only happens before the exit points of main method.
+        // Do not forget to insert instructions to report the counter
+        // this only happens before the exit points of main method.
 
-		// 1. check if this is the main method by checking signature
-		String signature = method.getSubSignature();
-		boolean isMain = signature.equals("void main(java.lang.String[])");
+        // 1. check if this is the main method by checking signature
+        String signature = method.getSubSignature();
+        boolean isMain = signature.equals("void main(java.lang.String[])");
 
-		// re-iterate the body to look for return statement
-		if (isMain) {
-			stmtIt = units.snapshotIterator();
+        // re-iterate the body to look for return statement
+        if (isMain) {
+            stmtIt = units.snapshotIterator();
 
-			while (stmtIt.hasNext()) {
-				Stmt stmt = (Stmt) stmtIt.next();
+            while (stmtIt.hasNext()) {
+                Stmt stmt = (Stmt) stmtIt.next();
 
-				// check if the instruction is a return with/without value
-				if ((stmt instanceof ReturnStmt)
-						|| (stmt instanceof ReturnVoidStmt)) {
-					// 1. make invoke expression of MyCounter.report()
-					InvokeExpr reportExpr = Jimple.v().newStaticInvokeExpr(
-							reportCounter.makeRef());
+                // check if the instruction is a return with/without value
+                if ((stmt instanceof ReturnStmt)
+                        || (stmt instanceof ReturnVoidStmt)) {
+                    // 1. make invoke expression of MyCounter.report()
+                    InvokeExpr reportExpr = Jimple.v().newStaticInvokeExpr(
+                            reportCounter.makeRef());
 
-					// 2. then, make a invoke statement
-					Stmt reportStmt = Jimple.v().newInvokeStmt(reportExpr);
+                    // 2. then, make a invoke statement
+                    Stmt reportStmt = Jimple.v().newInvokeStmt(reportExpr);
 
-					// 3. insert new statement into the chain
-					// (we are mutating the unit chain).
-					units.insertBefore(reportStmt, stmt);
-				}
-			}
-		}
-	}
+                    // 3. insert new statement into the chain
+                    // (we are mutating the unit chain).
+                    units.insertBefore(reportStmt, stmt);
+                }
+            }
+        }
+    }
 }
--- SoftwareAnalysis/Assignments/auto_rand_testing/src/main/java/profiler/MainDriver.java
+++ SoftwareAnalysis/Assignments/auto_rand_testing/src/main/java/profiler/MainDriver.java
@@ -6,27 +6,27 @@ import polyglot.main.Options;
 import soot.*;
 
 public class MainDriver {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
 		/* check the arguments */
-		if (args.length == 0) {
-			System.err.println("Usage: java MainDriver [options] classname");
-			System.exit(0);
-		}
+        if (args.length == 0) {
+            System.err.println("Usage: java MainDriver [options] classname");
+            System.exit(0);
+        }
 
 		/* add a phase to transformer pack by call Pack.add */
-		Pack jtp = PackManager.v().getPack("jtp");
-		jtp.add(new Transform("jtp.instrumenter",
-				new InvokeStaticInstrumenter()));
-		
+        Pack jtp = PackManager.v().getPack("jtp");
+        jtp.add(new Transform("jtp.instrumenter",
+                new InvokeStaticInstrumenter()));
+
 		/*
 		 * Give control to Soot to process all options,
 		 * InvokeStaticInstrumenter.internalTransform will get called.
 		 */
-		
-		for(String arg:args){
-			System.out.println(arg);
-		}
-		soot.Main.main(args);
-	}
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+        soot.Main.main(args);
+    }
 }
