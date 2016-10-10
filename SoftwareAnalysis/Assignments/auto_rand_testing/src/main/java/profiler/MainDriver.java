2451e678ed19a7fd9dd0978355039d69bffa8e6a
CHE Yulin <yche@cse.ust.hk>
2016-10-10 22:32:50 +0800

    add soot study

---
+++ SoftwareAnalysis/Assignments/auto_rand_testing/src/main/java/profiler/MainDriver.java
@@ -0,0 +1,32 @@
+package profiler;/* Usage: java MainDriver [soot-options] appClass
+ */
+
+import polyglot.main.Options;
+/* import necessary soot packages */
+import soot.*;
+
+public class MainDriver {
+	public static void main(String[] args) {
+
+		/* check the arguments */
+		if (args.length == 0) {
+			System.err.println("Usage: java MainDriver [options] classname");
+			System.exit(0);
+		}
+
+		/* add a phase to transformer pack by call Pack.add */
+		Pack jtp = PackManager.v().getPack("jtp");
+		jtp.add(new Transform("jtp.instrumenter",
+				new InvokeStaticInstrumenter()));
+		
+		/*
+		 * Give control to Soot to process all options,
+		 * InvokeStaticInstrumenter.internalTransform will get called.
+		 */
+		
+		for(String arg:args){
+			System.out.println(arg);
+		}
+		soot.Main.main(args);
+	}
+}
