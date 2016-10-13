594d9d2a808025c3d25fafb5585c171cdd8a22ad
cheyulin <yche@cse.ust.hk>
2016-10-13 10:41:31 +0800

    update randoop gen, not work

--- SoftwareAnalysis/Assignments/Assign1/UserfulShells/randoop_help.sh
+++ SoftwareAnalysis/Assignments/Assign1/UserfulShells/randoop_help.sh
@@ -1,2 +1,2 @@
 #$1 is command name, default is empty, $1 could be gentests
-java -ea -cp ../Local-Jars/randoop-all-3.0.4.jar randoop.main.Main help $1./us
+java -ea -cp ../Local-Jars/randoop-all-3.0.4.jar randoop.main.Main help $1
--- SoftwareAnalysis/Assignments/Assign1/UserfulShells/use_randoop_gen_tests.sh
+++ SoftwareAnalysis/Assignments/Assign1/UserfulShells/use_randoop_gen_tests.sh
@@ -1,4 +1,5 @@
 RANDOOP_CLASSPATH="../Local-Jars/randoop-all-3.0.4.jar"
 SRC_CLASSPATH="../AssignmentSubject/bin"
 IO_ARGS="--classlist=my_classes.txt --junit-output-dir=../AssignmentSubject/test_src"
-java -cp $RANDOOP_CLASSPATH:$SRC_CLASSPATH randoop.main.Main gentests $IO_ARGS
+LITERAL_ARGS="--literals-file=literals.txt"
+java -cp $RANDOOP_CLASSPATH:$SRC_CLASSPATH randoop.main.Main gentests $IO_ARGS $LITERAL_ARGS
