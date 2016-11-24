c13fb3fb6d5c8a83732f873b9f50288cc08e51e4
cheyulin <yche@cse.ust.hk>
2016-11-24 13:20:37 +0800

    add some codes to illustrate subject downloaded not correct

--- SoftwareAnalysis/Assignments/Assign2Subject/src/main/java/study/Study.java
+++ SoftwareAnalysis/Assignments/Assign2Subject/src/main/java/study/Study.java
@@ -1,18 +1,54 @@
 package study;
 
-import util.IntArrayUtil;
 
 /**
  * Created by cheyulin on 11/23/16.
  */
 
 public class Study {
-    public static void main(String[] args) {
-        IntArrayUtil arrayUtil = new IntArrayUtil();
-        error.IntArrayUtil arrayUtilError = new error.IntArrayUtil();
+
+    private static util.IntArrayUtil jarUtil = new util.IntArrayUtil();
+    private static error.IntArrayUtil srcUtil = new error.IntArrayUtil();
+
+    private static void printArray(int[] arr) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append('[');
+        for (int i = 0; i < arr.length; i++) {
+            stringBuilder.append(arr[i]);
+            if (i != arr.length - 1) {
+                stringBuilder.append(',');
+            }
+        }
+        stringBuilder.append(']');
+        System.out.println(stringBuilder.toString());
+
+    }
+
+    private static void studyFindMInViaIteration() {
+        System.out.println("\nstudyFindMInViaIteration");
         int[] arr = {11, 2, -3, 4, 5};
-        System.out.println(arrayUtil.findMinViaIteration(arr));
-        System.out.println(arrayUtilError.findMinViaIteration(arr));
+        System.out.println(jarUtil.findMinViaIteration(arr));
+        System.out.println(srcUtil.findMinViaIteration(arr));
+    }
+
+    private static void studyRemoveDuplicateElements() {
+        System.out.println("\nstudyRemoveDuplicateElements");
+        int[] num = {1, 2, 1, 3, 2, 0, 0};
+        int[] num2 = new int[num.length];
+        System.arraycopy(num, 0, num2, 0, num2.length);
+
+        System.out.println("jar util after removing:" + jarUtil.removeDuplicateElements(num));
+        printArray(num);
+
+        System.out.println("jar util after removing:" + srcUtil.removeDuplicateElements(num2));
+        printArray(num2);
+    }
+
+    
+    public static void main(String[] args) {
+        studyFindMInViaIteration();
+        studyRemoveDuplicateElements();
     }
 
 }
