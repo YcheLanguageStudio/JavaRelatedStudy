93c4830ac058dc081fe5223a8c6922c4ef56c257
cheyulin <yche@cse.ust.hk>
2016-11-29 19:20:54 +0800

    add gzoltar try

---
+++ SoftwareAnalysis/Assignments/Assign2/AssignmentSubject/test_src/janala/MinSubArrLenTestCase8.java
@@ -0,0 +1,23 @@
+package janala;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by cheyulin on 11/29/16.
+ */
+
+public class MinSubArrLenTestCase8 {
+    private static util.IntArrayUtil jarUtil = new util.IntArrayUtil();
+    private static tests.homework.IntArrayUtil srcUtil = new tests.homework.IntArrayUtil();
+
+    @Test
+    public void testMinSubArr() throws Exception {
+        int[] arr0 = {0, 0, 0, 0};
+        int[] arr1 = {0, 0, 0, 0};
+        int targetVal  = 9;
+
+        assertEquals(jarUtil.findMinSubArrayLen(targetVal, arr0), srcUtil.findMinSubArrayLen(targetVal, arr1));
+    }
+}
---
+++ SoftwareAnalysis/Assignments/Assign2/AssignmentSubject/test_src/janala/ThreeSumCloseTestCase52.java
@@ -0,0 +1,23 @@
+package janala;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by cheyulin on 11/29/16.
+ */
+public class ThreeSumCloseTestCase52 {
+
+    private static util.IntArrayUtil jarUtil = new util.IntArrayUtil();
+    private static tests.homework.IntArrayUtil srcUtil = new tests.homework.IntArrayUtil();
+
+    @Test
+    public void testThreeSumClose() throws Exception {
+        int[] arr0 = {0, 0, 0, 0, 0, 0, 0};
+        int[] arr1 = {0, 0, 0, 0, 0, 0, 0};
+        int targetVal  = -2147483647;
+
+        assertEquals(jarUtil.threeSumClosest(arr0, targetVal), srcUtil.threeSumClosest(arr1, targetVal));
+    }
+}
